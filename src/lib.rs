1c4d65330221c44c51e1ad9ff14b194f6054e4d2
Louis Maddox <louismmx@gmail.com>
2023-12-23 20:21:12 +0000

    feat: add optimisating release profile (no effect)

--- src/lib.rs
+++ src/lib.rs
@@ -5,31 +5,16 @@ use pyo3::exceptions;
 #[pyfunction]
 fn join_strings(_py: Python, string_list: &PyList, separator: Option<String>) -> PyResult<String> {
     let sep = separator.unwrap_or_else(|| "".to_string());
-    // Preallocate memory for the final string: calculate total length of all strings combined
-    let number_of_separators = string_list.len().saturating_sub(1);
-    let mut total_length = sep.len() * number_of_separators;
-
+    let mut strings = Vec::new();
+    
     for item in string_list.iter() {
         match item.extract::<String>() {
-            Ok(s) => total_length += s.len(),
+            Ok(s) => strings.push(s),
             Err(_) => return Err(exceptions::PyValueError::new_err("List items must be strings")),
         }
     }
 
-    // Then create a new string with this capacity (avoids reallocating memory while appending)
-    let mut result = String::with_capacity(total_length);
-
-    for (i, item) in string_list.iter().enumerate() {
-        let string = item.extract::<String>().unwrap(); // Safe to unwrap because we've already checked
-        result.push_str(&string);
-
-        // Add separator only if it's not the last element
-        if i < number_of_separators {
-            result.push_str(&sep);
-        }
-    }
-
-    Ok(result)
+    Ok(strings.join(&sep))
 }
 
 #[pymodule]
